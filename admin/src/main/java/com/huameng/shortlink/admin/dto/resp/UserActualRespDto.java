ad96d7d2fb94676fae948d510e8b9ecc097a42f6
realhuameng <1263313190@qq.com>
2024-02-08 11:15:52 +0800

    "开发短链接后管系统脱敏控制"

---
+++ admin/src/main/java/com/huameng/shortlink/admin/dto/resp/UserActualRespDto.java
@@ -0,0 +1,31 @@
+package com.huameng.shortlink.admin.dto.resp;
+
+import lombok.Data;
+
+@Data
+public class UserActualRespDto {
+    /**
+     * ID
+     */
+    private Long id;
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+}
