f015a95450f822f6b41edb7fc1cfb02a2a5f2a9e
realhuameng <1263313190@qq.com>
2024-02-12 18:17:35 +0800

    "用户实体信息优化"

--- admin/src/main/java/com/huameng/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/huameng/shortlink/admin/dao/entity/UserDO.java
@@ -1,18 +1,15 @@
 package com.huameng.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.huameng.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
-
-import java.util.Date;
 
 /**
  * 用户持久层实体
  */
 @Data
 @TableName("t_user")
-public class UserDO {
+public class UserDO extends BaseDO {
 
     /**
      * ID
@@ -49,23 +46,7 @@ public class UserDO {
     private Long deletionTime;
 
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
 
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0: 未删除 1：已删除
-     * */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
 
 
 
