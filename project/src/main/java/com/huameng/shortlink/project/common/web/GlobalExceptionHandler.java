cba30896bc95d05714ce91f4bac1cdf91ffe8d3b
realhuameng <1263313190@qq.com>
2024-02-19 18:48:46 +0800

    "开发短链接创建功能,添加布隆过滤器校验短链接是否重复"

---
+++ project/src/main/java/com/huameng/shortlink/project/common/web/GlobalExceptionHandler.java
@@ -0,0 +1,76 @@
+package com.huameng.shortlink.project.common.web;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
+import com.huameng.shortlink.project.common.convention.errorCode.BaseErrorCode;
+import com.huameng.shortlink.project.common.convention.exception.AbstractException;
+import com.huameng.shortlink.project.common.convention.result.Result;
+import com.huameng.shortlink.project.common.convention.result.Results;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+
+/**
+ * 全局异常处理器
+ *
+ */
+@Component
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 拦截参数验证异常
+     */
+    @SneakyThrows //将受检异常转换为非受检异常（unchecked exception），从而避免了在方法签名中声明或者在方法内部捕获受检异常的繁琐工作
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)//@ExceptionHandler 注解指定了这个方法用于处理 MethodArgumentNotValidException 类型的异常。当发生这种类型的异常时，Spring MVC 将会调用这个方法来处理异常。
+    public Result validExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        //获取绑定结果
+        BindingResult bindingResult = ex.getBindingResult();
+        //获取第一个字段错误
+        FieldError firstFieldError = CollectionUtil.getFirst(bindingResult.getFieldErrors());
+        String exceptionStr = Optional.ofNullable(firstFieldError)
+                .map(FieldError::getDefaultMessage)
+                .orElse(StrUtil.EMPTY);
+        log.error("[{}] {} [ex] {}", request.getMethod(), getUrl(request), exceptionStr);
+        return Results.failure(BaseErrorCode.CLIENT_ERROR.code(), exceptionStr);
+    }
+
+    /**
+     * 拦截应用内抛出的异常
+     */
+    @ExceptionHandler(value = {AbstractException.class})
+    public Result abstractException(HttpServletRequest request, AbstractException ex) {
+        if (ex.getCause() != null) {
+            log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString(), ex.getCause());
+            return Results.failure(ex);
+        }
+        log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex.toString());
+        return Results.failure(ex);
+    }
+
+    /**
+     * 拦截未捕获异常
+     */
+    @ExceptionHandler(value = Throwable.class)
+    public Result defaultErrorHandler(HttpServletRequest request, Throwable throwable) {
+        log.error("[{}] {} ", request.getMethod(), getUrl(request), throwable);
+        return Results.failure();
+    }
+
+    private String getUrl(HttpServletRequest request) {
+        if (StringUtils.isEmpty(request.getQueryString())) {
+            return request.getRequestURL().toString();
+        }
+        return request.getRequestURL().toString() + "?" + request.getQueryString();
+    }
+}
--- project/src/main/java/com/huameng/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/huameng/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,7 +1,9 @@
 package com.huameng.shortlink.project.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.huameng.shortlink.project.common.convention.exception.ServiceException;
 import com.huameng.shortlink.project.dao.entity.ShortLinkDO;
 import com.huameng.shortlink.project.dao.mapper.ShortLinkMapper;
 import com.huameng.shortlink.project.dto.req.ShortLinkCreateReqDto;
@@ -9,6 +11,9 @@ import com.huameng.shortlink.project.dto.resp.ShortLinkCreateRespDto;
 import com.huameng.shortlink.project.service.ShortLinkService;
 import com.huameng.shortlink.project.toolkit.HashUtil;
 import groovy.util.logging.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,14 +21,40 @@ import org.springframework.stereotype.Service;
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
+
+    private final RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter;
+
     @Override
     public ShortLinkCreateRespDto createShortLink(ShortLinkCreateReqDto requestParam) {
         String shortLinkSuffix = generateSuffix(requestParam);
-        ShortLinkDO shortLinkDO = BeanUtil.toBean(requestParam, ShortLinkDO.class);
-        shortLinkDO.setShortUri(shortLinkSuffix);
-        shortLinkDO.setFullShortUrl(requestParam.getDomain() + "/" + shortLinkSuffix);
-        baseMapper.insert(shortLinkDO);
+        String fullShortUrl = requestParam.getDomain() + "/" + shortLinkSuffix;
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .domain(requestParam.getDomain())
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .createdType(requestParam.getCreatedType())
+                .validDateType(requestParam.getValidDateType())
+                .validDate(requestParam.getValidDate())
+                .describe(requestParam.getDescribe())
+                .shortUri(shortLinkSuffix)
+                .enableStatus(0)
+                .fullShortUrl(fullShortUrl)
+                .build();
+        try{
+            baseMapper.insert(shortLinkDO);
+        }catch(DuplicateKeyException e){
+            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
+            ShortLinkDO shortLinkDO1 = baseMapper.selectOne(queryWrapper);
+            if(shortLinkDO1 != null){
+                log.warn("短链接：重复入库");
+                throw new ServiceException("短链接生成重复");
+            }
+
+        }
+        shortUriCreateCachePenetrationBloomFilter.add(shortLinkSuffix);
         return ShortLinkCreateRespDto.builder()
                 .fullShortUrl(shortLinkDO.getFullShortUrl())
                 .gid(requestParam.getGid())
@@ -32,7 +63,21 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     }
 
     private String generateSuffix(ShortLinkCreateReqDto requestParam){
-        String originUrl = requestParam.getOriginUrl();
-        return HashUtil.hashToBase62(originUrl);
+        int customGenerateCount = 0;
+        String shortUri;
+        while(true){
+            if(customGenerateCount > 10){
+                throw new ServiceException("短链接频繁生成，请稍后再试");
+            }
+            String originUrl = requestParam.getOriginUrl();
+            originUrl += System.currentTimeMillis();
+            shortUri = HashUtil.hashToBase62(originUrl);
+            if(!shortUriCreateCachePenetrationBloomFilter.contains(originUrl)){
+                break;
+            }
+            customGenerateCount++;
+        }
+
+        return shortUri;
     }
 }
